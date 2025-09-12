539b93aa00eab3a246d03c074feca845602ff0bd
luhvi <luhvilive@gmail.com>
2025-09-12 18:59:19 +0200

    fixed frontend

---
+++ frontend/app/page.tsx
@@ -0,0 +1,9 @@
+import { Graph } from "@/components/ui/graph";
+
+export default function Page() {
+  return (
+    <main className="flex items-center justify-center">
+      <Graph />
+    </main>
+  );
+}
