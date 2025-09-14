6a672e2827e2ad6863727c24a5286d012b0281a9
luhvi <luhvilive@gmail.com>
2025-09-14 16:47:44 +0200

    added price comparing to previous closing price

--- frontend/.next/static/chunks/app_page_tsx_2a6dfdfb._.js
+++ frontend/.next/static/chunks/app_page_tsx_2a6dfdfb._.js
@@ -1,8 +1,18 @@
 (globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
     script: typeof document === "object" ? document.currentScript : undefined,
     chunks: [
-  "static/chunks/node_modules_67c34ef5._.js",
-  "static/chunks/_0e3ee904._.js"
+  "static/chunks/node_modules_@mui_system_esm_7b375f58._.js",
+  "static/chunks/node_modules_@mui_material_esm_e7e35647._.js",
+  "static/chunks/node_modules_@mui_x-charts_esm_internals_b4977ed5._.js",
+  "static/chunks/node_modules_@mui_x-charts_esm_ChartContainer_2c08f2cb._.js",
+  "static/chunks/node_modules_@mui_x-charts_esm_ChartsTooltip_296f0d6e._.js",
+  "static/chunks/node_modules_@mui_x-charts_esm_a7f08ecd._.js",
+  "static/chunks/node_modules_robust-predicates_5981771c._.js",
+  "static/chunks/node_modules_@mui_x-internal-gestures_esm_core_2db3e4fd._.js",
+  "static/chunks/node_modules_@popperjs_core_lib_229f7621._.js",
+  "static/chunks/node_modules_axios_lib_99999129._.js",
+  "static/chunks/node_modules_eb28cc61._.js",
+  "static/chunks/_56f097dd._.js"
 ],
     source: "dynamic"
 });
---
+++ frontend/app/ticker/[symbol]/page.tsx
@@ -0,0 +1,112 @@
+"use client";
+
+import {
+  getSinglePrevTickerClose,
+  getSingleTickerHist,
+  getSingleTickerPrice,
+  HistDataPoint,
+  isValidTicker,
+} from "@/ticker-data";
+import { useEffect, useState, use } from "react";
+
+interface PageProps {
+  params: Promise<{
+    symbol: string;
+  }>;
+}
+
+export default function Page({ params }: PageProps) {
+  const resolvedParams = use(params);
+  const symbol = resolvedParams.symbol.toUpperCase();
+  const [hist, setHist] = useState<HistDataPoint[]>([]);
+  const [price, setPrice] = useState<number | null>(null);
+  const [prevClosePrice, setPrevClosePrice] = useState<number | null>(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
+
+  const corporationNameMap: Record<string, string> = {
+    NVDA: "NVIDIA Corporation",
+    MSFT: "Microsoft Corporation",
+    AAPL: "Apple Inc.",
+  };
+
+  const corporationName = corporationNameMap[symbol];
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const isValid = isValidTicker(symbol);
+      if (!isValid) {
+        console.error(`Invalid ticker ${symbol}`);
+        setError(`Invalid ticker ${symbol}`);
+        setLoading(false);
+        return;
+      }
+      try {
+        const [histData, priceData, prevClosePriceData] = await Promise.all([
+          getSingleTickerHist(symbol),
+          getSingleTickerPrice(symbol),
+          getSinglePrevTickerClose(symbol),
+        ]);
+        setHist(histData);
+        setPrice(priceData);
+        setPrevClosePrice(prevClosePriceData);
+        setError(null);
+        setLoading(false);
+        return;
+      } catch (error) {
+        console.error(`Error fetching ${symbol} data:`, error);
+        setError(`Error fetching ${symbol} data`);
+        setLoading(false);
+        return;
+      }
+    };
+    fetchData();
+  }, [symbol]);
+
+  const getPriceDifference = () => {
+    if (!price || !prevClosePrice) return null;
+    const priceDifference = price - prevClosePrice;
+    if (priceDifference < 0) {
+      const differencePercentage = (priceDifference / prevClosePrice) * 100;
+      const priceDifferenceObj = {
+        number: `${priceDifference.toFixed(2)}`,
+        percentage: `(-${differencePercentage.toFixed(2)}%)`,
+      };
+      return priceDifferenceObj;
+    } else {
+      const differencePercentage = (priceDifference / prevClosePrice) * 100;
+      const priceDifferenceObj = {
+        number: `+${priceDifference.toFixed(2)}`,
+        percentage: `+(${differencePercentage.toFixed(2)}%)`,
+      };
+      return priceDifferenceObj;
+    }
+  };
+
+  const priceDifferenceNumber = getPriceDifference()?.number;
+  const priceDifferencePercentage = getPriceDifference()?.percentage;
+
+  return (
+    <div>
+      <div className="flex items-center gap-2">
+        <span className="text-2xl font-semibold text-white">
+          {corporationName}
+        </span>
+        <span className="text-2xl font-semibold text-white">({symbol})</span>
+      </div>
+      <div className="flex items-center gap-2">
+        <span className="text-4xl font-bold text-white">{price}</span>
+        <span
+          className={`text-xl font-bold ${priceDifferenceNumber?.[0] === "+" ? "text-green-500" : "text-red-600"} `}
+        >
+          {priceDifferenceNumber}
+        </span>
+        <span
+          className={`text-xl font-bold ${priceDifferenceNumber?.[0] === "+" ? "text-green-500" : "text-red-600"} `}
+        >
+          {priceDifferencePercentage}
+        </span>
+      </div>
+    </div>
+  );
+}
