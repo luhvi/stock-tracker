499c423e9bdea33d6312f6e7859da9efaffb3c34
luhvi <luhvilive@gmail.com>
2025-09-15 20:36:58 +0200

    changed backdrop blur of menu

--- frontend/components/layout/menu.tsx
+++ frontend/components/layout/menu.tsx
@@ -24,7 +24,7 @@ export const Menu = () => {
   if (!isMenuOpen) return null;
 
   return (
-    <div className="fixed top-0 right-0 bottom-0 left-0 backdrop-blur-md">
+    <div className="fixed top-0 right-0 bottom-0 left-0 backdrop-blur-xs">
       <div className="mx-auto flex max-w-250 flex-col px-4 py-16">
         {MenuBtns.map((menuBtn, index) => (
           <MenuBtn key={index} title={menuBtn.title} href={menuBtn.href} />
